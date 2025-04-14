cf2efd22edf71f9db9b3e75a02ab43e53079a8b8
Vicky17 <33864061+shiqichen17@users.noreply.github.com>
2025-04-14 21:21:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-# export CUDA_VISIBLE_DEVICES='3,4'
-# export TEST_MODE=True
-# python3 main_aro.py --dataset=Controlled_Images_A --model-name='llava1.5' --download --method=scaling_vis --eval out --weight=0.8   --option=four
+# Add the following line to enable test mode; otherwise, it defaults to validation mode
+export TEST_MODE=True
+
+# Baseline: greedy decoding
+python3 main_aro.py --dataset=Controlled_Images_A --model-name='llava1.5' --download --method=base  --option=four
+
+# For Scaling_Vis on Controlled_A, a weight of 0.8 is used
+python3 main_aro.py --dataset=Controlled_Images_A --model-name='llava1.5' --download --method=scaling_vis  --weight=0.8  --option=four
+
+# For Adapt_Vis on Controlled_A, weight1 is set to 0.5, weight2 to 1.5, and threshold to 0.4
 python3 main_aro.py --dataset=Controlled_Images_A --model-name='llava1.5' --download --method adapt_vis --weight1 0.5  --weight2 1.5 --threshold 0.4 --option=four
